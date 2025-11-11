cd1ed52a2ab045a43a39746043a990a90d50feb7
Trinity Song <kyeongyeon.song@gmail.com>
2025-11-11 22:47:46 +0900

    뒤로가기 버튼 클릭시 앱 종료 안하는 문제 수정

--- ai-profile-photo/src/App.jsx
+++ ai-profile-photo/src/App.jsx
@@ -1,16 +1,14 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import ProfilePage from './pages/ProfilePage'
 import './App.css'
-
-const PROFILE_PATH = '/profile'
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navigate to={PROFILE_PATH} replace />} />
-        <Route path={PROFILE_PATH} element={<ProfilePage />} />
-        <Route path="*" element={<Navigate to={PROFILE_PATH} replace />} />
+        <Route path="/" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<ProfilePage />} />
       </Routes>
     </div>
   )
